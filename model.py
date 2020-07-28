70dda2b2c13e5417c6e1e012b3c266961da16815
SovSauce <sovsauce@gmail.com>
2020-07-28 19:25:51 +0000

    model.py

---
+++ model.py
@@ -0,0 +1,11 @@
+from model import getImageUrlFrom
+import requests
+
+
+def getImageUrlFrom(query, key):
+    search_url = "" + key + "" + query + ""
+    data = requests.get(search_url)
+    recipe_info = data.json()
+    img_url = recipe_info['data'][0]['images']['original']['url']
+    print (img_url)
+    return img_url
