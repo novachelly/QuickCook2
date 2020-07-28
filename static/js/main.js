2f54525d4386abec0a9a9aa7d5f39b60de7d3625
chelsea <chaye@brooklynfriends.org>
2020-07-28 15:21:18 -0400

    first commit

--- static/js/main.js
+++ static/js/main.js
@@ -13,3 +13,17 @@ window.addEventListener("scroll", function () {
     nav.classList.remove("scrolled");
   }
 })
+
+$(document).ready(function(){
+
+  $('.input').focus(function(){
+    $(this).parent().find(".label-txt").addClass('label-active');
+  });
+
+  $(".input").focusout(function(){
+    if ($(this).val() == '') {
+      $(this).parent().find(".label-txt").removeClass('label-active');
+    };
+  });
+
+});
