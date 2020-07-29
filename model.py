28c9de914415706cb052337868e8c7777f69c354
SovSauce <sovsauce@gmail.com>
2020-07-29 14:49:28 +0000

    spoonacular

--- model.py
+++ model.py
@@ -1,15 +1,17 @@
-<<<<<<< HEAD
-# from model import getImageUrlFrom
-# import requests
-=======
 import requests
->>>>>>> 51be09f14bb9122354ae835b0b9a9ee565b344a0
 
 
-# def getImageUrlFrom(query, key):
-#     search_url = "" + key + "" + query + ""
-#     data = requests.get(search_url)
-#     recipe_info = data.json()
-#     img_url = recipe_info['data'][0]['images']['original']['url']
-#     print (img_url)
-#     return img_url
+def getImageUrlFrom(query, key):
+    search_url = "https://spoonacular.com/recipeImages/" + ID + "-" + SIZE + "." + TYPE
+    # 90x90
+    # 240x150
+    # 312x150
+    # 312x231
+    # 480x360
+    # 556x370
+    # 636x393
+    data = requests.get("https://api.spoonacular.com/recipes/complexSearch")
+    recipe_info = data.json()
+    img_url = recipe_info['data'][0]['images']['original']['url']
+    print (img_url)
+    return img_url
