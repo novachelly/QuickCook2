51be09f14bb9122354ae835b0b9a9ee565b344a0
SovSauce <sovsauce@gmail.com>
2020-07-29 14:09:50 +0000

    app.run error

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template
 from flask import request
 from datetime import datetime
-from model import getImageUrlFrom
+# from model import getImageUrlFrom
 import os
 
 app = Flask(__name__)
@@ -10,26 +10,27 @@ app.config['SPOON_KEY'] = os.getenv('SPOON_KEY')
 
 
 @app.route('/')
+@app.route('/index')
 def index():
     return render_template('index.html', time=datetime.now())
 
 
-app.run('0.0.0.0', 8080)
+if __name__ == "__main__":
+    app.run('0.0.0.0', 8080)
 
 
-@app.route('/recipes', methods=['GET', 'POST'])
+@app.route('/recipes')
 def recipes():
-    if request.method == "GET":
-        return render_template('recipes.html', time=datetime.now())
+    return render_template('recipes.html', time=datetime.now())
 
 
-@app.route('/recipes_results', methods=['GET', 'POST'])
-def recipes_results():
-    if request.method == 'POST':
-        query = request.form['recipes']
-        key = app.config['SPOON_KEY']
-        img_link = getImageUrlFrom(query, key)
-        return render_template('recipes_results.html', img_link=img_link, query=query)
+# @app.route('/recipes_results', methods=['GET', 'POST'])
+# def recipes_results():
+#     if request.method == 'POST':
+#         query = request.form['recipes']
+#         key = app.config['SPOON_KEY']
+#         img_link = getImageUrlFrom(query, key)
+#         return render_template('recipes_results.html', img_link=img_link, query=query)
 
 
 @app.route('/aboutus')
--- model.py
+++ model.py
@@ -1,4 +1,3 @@
-from model import getImageUrlFrom
 import requests
 
 
