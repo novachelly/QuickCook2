746a43d94a031e47aaa2ef2330e5dd2e491ac5e3
SovSauce <sovsauce@gmail.com>
2020-07-30 18:39:42 +0000

    save

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ import requests
 #     print (img_url)
 #     return img_url
 
-def getRecipeFromIngredients(ingredients, number, key, missedIngredients):
+def getRecipeFromIngredients(ingredients, number, key):
     search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
     data = requests.get(search_url)
     recipe_info = data.json()
