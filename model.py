7b5bed1fcee8e45ce71fdba365887e423494b8fc
SovSauce <sovsauce@gmail.com>
2020-07-31 18:13:08 +0000

    eek

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import requests
-
+import app
+from app import recipes_results
 
 # def getRecipeFromIngredients(ingredients, number, key):
 #     search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
@@ -49,152 +50,182 @@ def getRecipeInfo(id_number, key):
     recipe_info = data.json()
     return recipe_info
 
+if recipes_results.number >= 1:
+    def getRecipeFromIngredients1(ingredients, number, key):
+        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info[1]["id"]
 
-def getRecipeFromIngredients1(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[1]["id"]
+    def getRecipeInfo1(id_number, key):
+        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info
+        if recipes_results.number >= 2:
+            def getRecipeFromIngredients2(ingredients, number, key):
+                search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+                data = requests.get(search_url)
+                recipe_info = data.json()
+                return recipe_info[2]["id"]
 
+            def getRecipeInfo2(id_number, key):
+                search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+                # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+                data = requests.get(search_url)
+                recipe_info = data.json()
+                return recipe_info
+                if recipes_results.number >= 3:
+                    def getRecipeFromIngredients3(ingredients, number, key):
+                        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+                        data = requests.get(search_url)
+                        recipe_info = data.json()
+                        return recipe_info[3]["id"]
 
-def getRecipeInfo1(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
+                    def getRecipeInfo3(id_number, key):
+                        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+                        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+                        data = requests.get(search_url)
+                        recipe_info = data.json()
+                        return recipe_info
+                        if recipes_results.number >= 4:
+                            def getRecipeFromIngredients4(ingredients, number, key):
+                                search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+                                data = requests.get(search_url)
+                                recipe_info = data.json()
+                                return recipe_info[4]["id"]
 
+                            def getRecipeInfo4(id_number, key):
+                                search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+                                # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+                                data = requests.get(search_url)
+                                recipe_info = data.json()
+                                return recipe_info
+                                if recipes_results.number >= 5:
+                                    def getRecipeFromIngredients5(ingredients, number, key):
+                                        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+                                        data = requests.get(search_url)
+                                        recipe_info = data.json()
+                                        return recipe_info[5]["id"]
 
-def getRecipeFromIngredients2(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[2]["id"]
+                                    def getRecipeInfo5(id_number, key):
+                                        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+                                        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+                                        data = requests.get(search_url)
+                                        recipe_info = data.json()
+                                        return recipe_info
+                                        if recipes_results.number >= 5:
+                                            def getRecipeFromIngredients6(ingredients, number, key):
+                                                search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+                                                data = requests.get(search_url)
+                                                recipe_info = data.json()
+                                                return recipe_info[6]["id"]
 
+                                            def getRecipeInfo6(id_number, key):
+                                                search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+                                                # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+                                                data = requests.get(search_url)
+                                                recipe_info = data.json()
+                                                return recipe_info
+                                        else:
+                                            print("Hope you found what you needed! :)")
 
-def getRecipeInfo2(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
+                                        
+                                else:
+                                    print("Hope you found what you needed! :)")
+                        else:
+                            print("Hope you found what you needed! :)")
 
+                else:
+                    print("Hope you found what you needed! :)")
 
-def getRecipeFromIngredients3(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[3]["id"]
+        else:
+            print("Hope you found what you needed! :)")
 
-
-def getRecipeInfo3(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
-
-
-def getRecipeFromIngredients4(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[4]["id"]
-
-
-def getRecipeInfo4(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
-
-
-def getRecipeFromIngredients5(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[5]["id"]
-
-
-def getRecipeInfo5(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
-
-
-def getRecipeFromIngredients6(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[6]["id"]
-
-
-def getRecipeInfo6(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
-
-
-def getRecipeFromIngredients7(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[7]["id"]
-
-
-def getRecipeInfo7(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
+else:
+    print("Hope you found what you needed! :)")
 
 
-def getRecipeFromIngredients8(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[8]["id"]
 
 
-def getRecipeInfo8(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
 
+if recipes_results.number >= 5:
+    def getRecipeFromIngredients6(ingredients, number, key):
+        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info[6]["id"]
 
-def getRecipeFromIngredients9(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[9]["id"]
+    def getRecipeInfo6(id_number, key):
+        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info
+else:
+    print("Hope you found what you needed! :)")
 
+if recipes_results.number >= 7:
+    def getRecipeFromIngredients7(ingredients, number, key):
+        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info[7]["id"]
 
-def getRecipeInfo9(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
+    def getRecipeInfo7(id_number, key):
+        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info
+else:
+    print("Hope you found what you needed! :)")
 
+if recipes_results.number >= 8:
+    def getRecipeFromIngredients8(ingredients, number, key):
+        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info[8]["id"]
 
-def getRecipeFromIngredients10(ingredients, number, key):
-    search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info[10]["id"]
+    def getRecipeInfo8(id_number, key):
+        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info
+else:
+    print("Hope you found what you needed! :)")
 
+if recipes_results.number >= 9:
+    def getRecipeFromIngredients9(ingredients, number, key):
+        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info[9]["id"]
 
-def getRecipeInfo10(id_number, key):
-    search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
-    # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
-    data = requests.get(search_url)
-    recipe_info = data.json()
-    return recipe_info
+    def getRecipeInfo9(id_number, key):
+        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info
+else:
+    print("Hope you found what you needed! :)")
+
+if recipes_results.number >= 10:
+    def getRecipeFromIngredients10(ingredients, number, key):
+        search_url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=" + key + "&ingredients=" + ingredients + "&number=" + number
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info[10]["id"]
+
+    def getRecipeInfo10(id_number, key):
+        search_url = "https://api.spoonacular.com/recipes/" + str(id_number) + "/information?apiKey=" + key
+        # search_url = f'https://api.spoonacular.com/recipes/{id_number}/information?apiKey={key}'
+        data = requests.get(search_url)
+        recipe_info = data.json()
+        return recipe_info
+else:
+    print("Hope you found what you needed! :)")
